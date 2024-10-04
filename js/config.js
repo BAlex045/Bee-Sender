bcb65a5c237069610b90e99ac72e81c8d0aac70f
BAlex045 <165044819+BAlex045@users.noreply.github.com>
2024-10-04 21:24:29 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@
 // change this address to your own address
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x609C2DF84419C4a66C261E3D013c593ac30CB0A9";
 
+var BaseFee=0;
